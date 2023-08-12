48b4fd31b4213191c7d498d0975a00084455b8a1
felixary_ <felixarydwisaputra@gmail.com>
2023-08-12 12:00:40 +0700

    final web UI

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+// ubah background header
+function ubahBackground() {
+    const header = document.getElementById('header');
+    if(this.scrollY >=50) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', ubahBackground);
+
+// swiper scrool
+var swipePopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursur: true,
+    centeredSlides: true,
+    slidesPerview: 'auto',
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+// value accordion
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem !== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
+
+// scrool up
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+    if(this.scrollY >= 350)scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
